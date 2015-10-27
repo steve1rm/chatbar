54920486fcc76913454829b074c67955cd434245
steve <steve@sun-system.com>
2015-10-27 16:30:55 +0700

    minor adjustments

--- app/src/main/java/com/ssd/chatbar/MainActivity.java
+++ app/src/main/java/com/ssd/chatbar/MainActivity.java
@@ -2,6 +2,11 @@ package com.ssd.chatbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tvMessageCounter = (TextView)findViewById(R.id.tvMessageCounter);
+        final Button btnIncrement = (Button)findViewById(R.id.btnIncreamentMsgAlert);
+
+        btnIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* Test the increment of the message alerts */
+                Integer counter = Integer.valueOf(tvMessageCounter.getText().toString());
+                counter++;
+                tvMessageCounter.setText(String.format(Locale.getDefault(), "%d", counter));
+            }
+        });
     }
 }
